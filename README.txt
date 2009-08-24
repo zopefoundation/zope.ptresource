4d6ab200b371677b979fb353b3e3b53b35c6488a
Dan Korostelev <nadako@gmail.com>
2009-08-24 12:53:58 +0000

    Move out of sandbox.

---
+++ README.txt
@@ -0,0 +1,11 @@
+========
+Overview
+========
+
+*This package is at present not reusable without depending on a large
+chunk of the Zope Toolkit and its assumptions. It is maintained by the*
+`Zope Toolkit project <http://docs.zope.org/zopetoolkit/>`_.
+
+This package provides a "page template" resource class, a resource which
+content is processed with Zope Page Templates engine before returning to
+client.
---
+++ setup.py
@@ -0,0 +1,54 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""zope.ptresource setup
+"""
+from setuptools import setup, find_packages, Extension
+
+long_description = (open('README.txt').read() + '\n\n' +
+                    open('CHANGES.txt').read())
+
+setup(name='zope.ptresource',
+      version = '3.9.0dev',
+      url='http://pypi.python.org/pypi/zope.ptresource/',
+      author='Zope Corporation and Contributors',
+      author_email='zope-dev@zope.org',
+      classifiers = ['Environment :: Web Environment',
+                     'Intended Audience :: Developers',
+                     'License :: OSI Approved :: Zope Public License',
+                     'Programming Language :: Python',
+                     'Operating System :: OS Independent',
+                     'Topic :: Internet :: WWW/HTTP',
+                     'Framework :: Zope3',
+                     ],
+      description='Page template resource plugin for zope.browserresource',
+      long_description=long_description,
+
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+
+      namespace_packages=['zope'],
+      include_package_data=True,
+      install_requires=['setuptools',
+                        'zope.browserresource',
+                        'zope.interface',
+                        'zope.pagetemplate',
+                        'zope.publisher',
+                        'zope.security[untrustedpython]',
+                        ],
+      extras_require={
+          'test': ['zope.testing'],
+          },
+
+      zip_safe = False,
+      )

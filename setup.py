a9d23ed55d2b49a97d1a2a921c70f0350b5cda90
Michael Howitz <mh@gocept.com>
2021-12-15 08:04:51 +0100

    Preparing release 4.3.0

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ TESTS_REQUIRE = [
 
 setup(
     name='zope.ptresource',
-    version='4.3.0.dev0',
+    version='4.3.0',
     url='https://github.com/zopefoundation/zope.ptresource/',
     project_urls={
         'Issue Tracker': ('https://github.com/zopefoundation/'

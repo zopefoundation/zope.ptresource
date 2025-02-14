6e9579fc62ded4355b8fa112e80e33a1391925fb
Michael Howitz <icemac@gmx.net>
2025-02-14 09:39:20 +0100

    Preparing release 5.1

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ TESTS_REQUIRE = [
 
 setup(
     name='zope.ptresource',
-    version='5.1.dev0',
+    version='5.1',
     url='https://github.com/zopefoundation/zope.ptresource/',
     project_urls={
         'Issue Tracker': ('https://github.com/zopefoundation/'
